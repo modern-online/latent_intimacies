99f808f9cab93c92c2540bc92e730056589a0f70
modern-online <53079544+modern-online@users.noreply.github.com>
2024-02-03 15:58:36 +0100

    Add files via upload

---
+++ cat/camera_test.py
@@ -0,0 +1,29 @@
+import cv2
+
+# Open a camera (0 is usually the default camera)
+cap = cv2.VideoCapture(0)
+
+# Check if the camera is opened successfully
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+while True:
+    # Read a frame from the camera
+    ret, frame = cap.read()
+
+    # Check if the frame is captured successfully
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+
+    # Display the frame in a window
+    cv2.imshow("Video Stream", frame)
+
+    # Break the loop if 'Esc' key is pressed
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+# Release the camera capture object and close the window
+cap.release()
+cv2.destroyAllWindows()
---
+++ cat/tts_test.py
@@ -0,0 +1,11 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.setProperty('rate', 115)
+voices = engine.getProperty('voices')  
+for i, voice in enumerate(voices):
+    print(i, voice.id, voice.name)
+engine.setProperty('voice', voices[113].id)
+## 15 Brian (UK)
+
+engine.say("I will speak this text")
+engine.runAndWait()
