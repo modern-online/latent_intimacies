41604ad3a7cffbcacee95993e8ecf698714286c2
modern-online <53079544+modern-online@users.noreply.github.com>
2024-02-05 15:13:58 +0100

    camera test mod

--- cat/camera_test.py
+++ cat/camera_test.py
@@ -2,6 +2,8 @@
 
 import cv2
 
+image_path = 'data/image.jpg'
+
 # Open a camera (0 is usually the default camera)
 cap = cv2.VideoCapture(0)
 
@@ -10,21 +12,14 @@ if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
 
-while True:
-    # Read a frame from the camera
-    ret, frame = cap.read()
+# Capture a single frame
+for iter in range(3):
+    _, frame = cap.read()
 
-    # Check if the frame is captured successfully
-    if not ret:
-        print("Error: Could not read frame.")
-        break
+ # Save the frame
+cv2.imwrite(image_path, frame)
 
-    # Display the frame in a window
-    cv2.imshow("Video Stream", frame)
-
-    # Break the loop if 'Esc' key is pressed
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+cap.release()
 
 # Release the camera capture object and close the window
 cap.release()
