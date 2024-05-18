f9094b8feaf79905934be9a6d3c520e584ccf1b5
modern-online <53079544+modern-online@users.noreply.github.com>
2024-05-18 17:38:46 +0200

    Delete demolition/transl_to_list.py

--- demolition/transl_to_list.py
+++
@@ -1,9 +0,0 @@
-with open("./dirty_data/translated_data.txt", encoding="utf8") as text:
-    dataset = text.read().split('\n')
-
-for i, data in enumerate(dataset):
-      if not data:
-            dataset.pop(i)
-
-with open("./dirty_data/translated_list.txt", "w") as f:
-        f.write(str(dataset))
