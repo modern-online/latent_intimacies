33aa4ec3a229fd901a67e463392e17ad1dfd945e
modern-online <53079544+modern-online@users.noreply.github.com>
2024-05-18 17:32:08 +0200

    Create hugging_bark_test.py

---
+++ demolition/testing_utils/hugging_bark_test.py
@@ -0,0 +1,21 @@
+from transformers import AutoProcessor, BarkModel
+from scipy.io.wavfile import write as write_wav
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+processor = AutoProcessor.from_pretrained("suno/bark-small")
+model = BarkModel.from_pretrained("suno/bark-small", torch_dtype=torch.float32).to(device)
+sample_rate = model.generation_config.sample_rate
+model.enable_cpu_offload()
+model =  model.to_bettertransformer()
+
+inputs = processor(["Hello, my dog is cute"], return_tensors="pt").to(device)
+
+audio_array = model.generate(**inputs, do_sample=True)
+print(audio_array)
+audio_array = audio_array.cpu().numpy().squeeze()
+print(audio_array)
+
+
+write_wav("tts.wav", rate=sample_rate, data=audio_array)
