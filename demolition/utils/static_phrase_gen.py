8fc0e0008cd4c11419d94e929d305f183a9cd1f8
modern-online <53079544+modern-online@users.noreply.github.com>
2024-05-18 17:36:02 +0200

    Create static_phrase_gen.py

---
+++ demolition/utils/static_phrase_gen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+import sounddevice as sd
+import soundfile as sf
+import os
+import time
+
+os.environ["SUNO_OFFLOAD_CPU"] = "True"
+
+# download and load all models
+print("LOADING MDOELS")
+preload_models()
+
+thinking_prompt = "thinking"
+positive_prompt = "thank you, my answer will be remembered"
+negative_prompt = "my answer has been forgotten"
+satisfaction_query = "were you sattisfied with my answer, yes or no?"
+
+def generate_tts(index, prompt, name):
+    audio_array = generate_audio(prompt)
+    # save audio to disk
+    write_wav(f'{name}{index}.wav', SAMPLE_RATE, audio_array)
+
+print("GENERATING")
+# generate audio from text
+
+for index in range(1,5):
+    generate_tts(index=index, prompt=thinking_prompt, name="thinking")
+    generate_tts(index=index, prompt=positive_prompt, name="positive")
+    generate_tts(index=index, prompt=negative_prompt, name="negative")
+    generate_tts(index=index, prompt=satisfaction_query, name="satisfaction")
+
+print("Done")
